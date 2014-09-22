cf3856845b71acba78cd2871c7ce1b242f08afd6
winse <winseliu@foxmail.com>
2014-09-23 00:28:54 +0800

    优化调试接口!

--- test/test.js
+++ test/test.js
@@ -1,43 +1,25 @@
-var __modules = require("bmobcloud-local");
-var Response = function (done) {
-    var model = {};
-    model.end = function (data) {
-        console.log(data);
-        done && done();
-    }
-
-    return model;
-}
-
-var Request = function (param) {
-    var model = {};
-    model.body = param;
-
-    return model;
-}
+var tool = require("bmobcloud-local");
 
 //请根据自己的情况修改application_id和rest_key信息
 var options = {
     "app_key": "69015a79796397f7701454336b84e0c4",
-    "rest_key": "9b82ae3876ed4ee081d3c34224ff3a7a",
-    //"master_key": "501ed5a81380df115d47c14b9a2c7094"
+    "rest_key": "9b82ae3876ed4ee081d3c34224ff3a7a"
 };
 
-var Bmob = __modules.Bmob;
-Bmob.initialize(options.app_key, options.rest_key);
+tool.initialize(options.app_key, options.rest_key);
 
 //调用hello.js云端代码
-var hello = require("../cloud/hello.js").hello;
-hello(new Request(),new Response(),__modules);
+var hello = require("../../bmobcloud-local/test/hello.js").hello;
+tool.test(hello);
 
 //调用first.js云端代码（带上传参数）
 var first = require("../cloud/first.js").first;
-first(new Request({"name":"Bmob后端云"}),new Response(),__modules);
+tool.test(first, {"name":"Bmob后端云"});
 
 //调用second.js云端代码
 var second = require("../cloud/second.js").second;
-second(new Request(),new Response(),__modules);
+tool.test(second);
 
 //调用third.js云端代码（带上传参数）
 var third = require("../cloud/third.js").third;
-third(new Request({"userid":"y6qBDvXj"}),new Response(),__modules);
+tool.test(third, {"userid":"y6qBDvXj"});
